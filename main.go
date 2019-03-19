3ba68ef4fc586ae1178902dd915c849083a32651
Serhii Betsmanovskyi <48160048+serhii-dog@users.noreply.github.com>
2019-03-19 14:18:25 +0200

    Update main.go

--- main.go
+++ main.go
@@ -45,8 +45,9 @@ func main() {
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
 	host, _ := os.Hostname()
+	version, _ := os.Getenv("VERSION")
 	fmt.Fprintf(w, "Hello, world!\n")
-	fmt.Fprintf(w, "Version: 1.0.111\n")
+	fmt.Fprintf(w, "Version: %s\n", version)
 	fmt.Fprintf(w, "Hostname: %s\n", host)
 }
 // [END all]
